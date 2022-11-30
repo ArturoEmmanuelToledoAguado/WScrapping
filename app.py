edefd6b4f3d082e7db99b32924fa6b6639397bd6
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-29 22:16:03 -0600

    Archivos iniciales

---
+++ app.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+# Setting the variable `website` to the string `"https://es-la.facebook.com/"`
+website="https://es-la.facebook.com/"
+
+# Setting the variable `path` to the string `'C:\Users\toled\Downloads/chromedriver_win32/chromedriver'`
+path = 'C:\Users\toled\Downloads/chromedriver_win32/chromedriver'
+
+# Creating a new instance of the Chrome webdriver.
+driver = webdriver.Chrome()
