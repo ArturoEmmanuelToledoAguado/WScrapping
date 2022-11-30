e7da4eb750618ee34090db4120b3bbb4896f2820
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-29 22:52:15 -0600

    Abrir

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 # Setting the variable `website` to the string `"https://es-la.facebook.com/"`
 website="https://es-la.facebook.com/"
 
 # Setting the variable `path` to the string `'C:\Users\toled\Downloads/chromedriver_win32/chromedriver'`
-path = 'C:\Users\toled\Downloads/chromedriver_win32/chromedriver'
+path = 'C:/Users/toled/Downloads/chromedriver_win32/chromedriver.exe'
 
 # Creating a new instance of the Chrome webdriver.
-driver = webdriver.Chrome()
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
