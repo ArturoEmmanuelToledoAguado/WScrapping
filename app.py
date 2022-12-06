9980a780344479b052fbe5824d215bbc8b94e563
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-06 00:37:07 -0600

    Falta descargar

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-#import pyautogui
+import requests
+import pyautogui
 
 # Setting the variable `website` to the string `"https://es-la.facebook.com/"`
 website="https://repositorio.unam.mx/contenidos?f=883.%23.%23.a_lit:Repositorio%20de%20la%20Direcci%C3%B3n%20General%20de%20Bibliotecas%20y%20Servicios%20Digitales%20de%20Informaci%C3%B3n"
@@ -27,7 +28,17 @@ def Search():
     driver.maximize_window()
     document = driver.find_element(by= By.CLASS_NAME , value='element-ing-data-record' and 'img-portada')
     document.click()
-    time.sleep(400)
+    driver.minimize_window()
+    driver.maximize_window()
+    driver.minimize_window()
+    driver.maximize_window()
+    download = driver.find_element(by= By.ID, value='cont-completo')
+    download.click()
+    driver.implicitly_wait(20.20)
+    desc =driver.find_element(by= By.XPATH, value='//*[@id="icon"]/iron-icon')
+    desc.click()
+    driver.implicitly_wait(20.20)
+    time.sleep(200)
 
 
 OpenWebSite()
