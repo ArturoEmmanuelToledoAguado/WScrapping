0083efa01fc9a5f4614215ab14371ac0f0f9ca20
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-06 00:11:30 -0600

    Click resuelto

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+#import pyautogui
 
 # Setting the variable `website` to the string `"https://es-la.facebook.com/"`
-website="https://es-la.facebook.com/"
+website="https://repositorio.unam.mx/contenidos?f=883.%23.%23.a_lit:Repositorio%20de%20la%20Direcci%C3%B3n%20General%20de%20Bibliotecas%20y%20Servicios%20Digitales%20de%20Informaci%C3%B3n"
 
 # Setting the variable `path` to the string `'C:\Users\toled\Downloads/chromedriver_win32/chromedriver'`
 path = 'C:/Users/toled/Downloads/chromedriver_win32/chromedriver.exe'
@@ -12,13 +13,23 @@ path = 'C:/Users/toled/Downloads/chromedriver_win32/chromedriver.exe'
 # Creating a new instance of the Chrome webdriver.
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
-driver.get(website)
-driver.maximize_window()
-email = driver.find_element(by=By.ID,value='email')
-email.send_keys('jaames11@hotmail.com')
-password = driver.find_element(by=By.ID, value='pass')
-password.send_keys('Wellneverdie.201')
-login = driver.find_element(by= By.NAME, value='login')
-login.click()
-time.sleep(100)
-#input()
+
+def OpenWebSite():
+    driver.get(website)
+    driver.maximize_window()
+    
+def Search():
+    search = driver.find_element(by=By.ID,value='input-search')
+    search.send_keys('PNL')
+    find = driver.find_element(by= By.ID, value='btn-general-buscar')
+    find.click()
+    driver.minimize_window()
+    driver.maximize_window()
+    document = driver.find_element(by= By.CLASS_NAME , value='element-ing-data-record' and 'img-portada')
+    document.click()
+    time.sleep(400)
+
+
+OpenWebSite()
+Search()
+print("Holi")
