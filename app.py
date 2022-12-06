efc0b76ab50d7ad891f0922038b6ea99d5c79002
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-05 23:47:43 -0600

    Time resuelto

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
 # Setting the variable `website` to the string `"https://es-la.facebook.com/"`
 website="https://es-la.facebook.com/"
@@ -11,3 +13,12 @@ path = 'C:/Users/toled/Downloads/chromedriver_win32/chromedriver.exe'
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 driver.get(website)
+driver.maximize_window()
+email = driver.find_element(by=By.ID,value='email')
+email.send_keys('jaames11@hotmail.com')
+password = driver.find_element(by=By.ID, value='pass')
+password.send_keys('Wellneverdie.201')
+login = driver.find_element(by= By.NAME, value='login')
+login.click()
+time.sleep(100)
+#input()
